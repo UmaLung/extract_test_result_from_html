96dfcf539f931002e5aed191c6622094bd4e7f91
UmaLung <ulung@atmosic.com>
2021-12-03 16:43:22 +0800

    modify extract_all_items.py

--- extract_all_items.py
+++ extract_all_items.py
@@ -1,19 +1,22 @@
-data = []
+html_file_name = 'input_HTML.html'
+csv_file_name = html_file_name.replace('html', 'csv')
 
-with open('input_HTML.html', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        data.append(line.strip().replace('<td>', '').replace('</td>', '').replace('<sub>', '').replace('</sub>', ''))
-# print(len(data))
+def extract_item ():
+    data = []
+    with open(html_file_name, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            data.append(line.strip().replace('<td>', '').replace('</td>', '').replace('<sub>', '').replace('</sub>', ''))
 
-res = [x for x, z in enumerate(data) if '</tr>' in z]   # use '</tr> as seperator'
-# print(len(res))
-with open('item.csv', 'w', encoding = 'utf-8') as f:
-        for i in range(len(res) - 1):
-            item = ''
-            for n in range(2, 13):
-                item += data[res[i] + n ] + ','
-            # print(item)
-            f.write(item + '\n')
+    res = [x for x, z in enumerate(data) if '</tr>' in z]   # use '</tr> as anchor'
+    with open(csv_file_name, 'w', encoding = 'utf-8') as f:
+            for i in range(len(res) - 1):
+                item = ''
+                for n in range(2, 13):
+                    item += data[res[i] + n ] + ','
+                # print(item)
+                f.write(item + '\n')
 
 
     
+if __name__ == '__main__':
+     extract_item ()
