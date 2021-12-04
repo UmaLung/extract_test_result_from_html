1a5fba6bb3477f0959a8a9cd98c508bab502a254
UmaLung <ulung@atmosic.com>
2021-12-04 16:43:29 +0800

    final version for translate .html to .csv

--- extract_all_items.py
+++ extract_all_items.py
@@ -1,26 +1,66 @@
-
-def extract_item (html_file_name):
-    csv_file_name = html_file_name.replace('html', 'csv')
+def read_html_file(html_file_name):
     data = []
-    with open('input_HTML.html', 'r', encoding = 'utf-8') as f:
-        for line in f:
-            data.append(line.strip().replace('<td>', '').replace('</td>', '').replace('<sub>', '').replace('</sub>', ''))
-        print(len(data))
+    with open(html_file_name, 'r', encoding = 'utf-8') as f:
+        for line in f:  # remove unnecessary lines
+            data.append(line)
+    return data
 
-    res = [x for x, z in enumerate(data) if '</tr>' in z]   # use '</tr> as seperator'
-    print(len(res))
-    with open('item.csv', 'w', encoding = 'utf-8') as f:
-        for i in range(len(res) - 1):
-            item = ''
-            for n in range(2, 13):
-                item += data[res[i] + n ] + ','
-            # print(item)
-            f.write(item + '\n')
+def extract_wanted_lines(data):
+    new = []
+    for line in data:
+        if '<h1>' in line:
+            new.append(line.strip().replace('<h1>', '***'))
+        elif '<th>' in line:
+            new.append(line.strip().replace('<th>', '').replace('</th>', ''))        
+        elif '<td>' in line:
+            new.append(line.strip().replace('<td>', '').replace('</td>', ''))
+        else:
+            continue        
+    return new
+
+def concatenate_items(new):
+    merge = []
+    item = None
+    for line in new:
+        if '***' in line:
+            merge.append(line.replace('</h1>', '***'))
+            continue
+        elif '_LE_CA_' in line:
+            item = line + ','
+            continue
+        elif 'Test' in line:
+            item = line + ','
+            continue
+        elif 'PASS' in line:
+            merge.append(item + line)
+            item = None
+            continue
+        elif 'FAIL' in line:
+            merge.append(item + line)
+            item = None
+            continue
+        elif 'Result' in line:
+            merge.append(item + line)
+            item = None
+            continue
+        if item:
+            item += line + ','
+    return merge
+
+
+def save_to_csv(csv_file_name, merge):
+    with open(csv_file_name, 'w', encoding = 'utf-8-sig') as f:
+        for line in merge:  # remove unnecessary lines
+            f.write(line + '\n')
 
 
 if __name__ == '__main__':
     import sys
-    # extract_item(sys.argv[1])
-    extract_item('input_HTML.html')
+    html_file_name = sys.argv[1]
+    csv_file_name = html_file_name.replace('html', 'csv')
+    data = read_html_file(html_file_name)
+    new = extract_wanted_lines(data)
+    merge = concatenate_items(new)
+    save_to_csv(csv_file_name, merge)
 
     
